bcb940e3c7dd31083f4566a7d9ebb292bfc30967
lekicmilos <lekicm2019@gmail.com>
2024-04-19 19:47:47 +0200

    dodata izmena sefa, zamenika. neke optimizacije

--- projekat/app/Repositories/KatedraRepository.php
+++ projekat/app/Repositories/KatedraRepository.php
@@ -22,47 +22,7 @@ class KatedraRepository
         $this->katedra = $katedra;
     }
 
-    public function upsert(KatedraDTO $data)
-    {
-        $katedra = Katedra::find($data->id);
-        if (empty($katedra)) {
-            $katedra = new $this->katedra;
-        }
 
-        $katedra->naziv_katedre = $data->naziv;
-        $katedra->aktivna = true;
-
-        $katedra->save();
-
-        /*$zaposleni = [];
-        foreach ($data->zaposleni as $zap)
-        {
-            $zaposleni[$zap->id] = [
-                'datum_od' => $zap->datum_od,
-                'datum_do' => $zap->datum_do
-            ];
-        }
-
-
-        $katedra->angazovanje()->attach($zaposleni);*/
-
-        // da li sef i zamenik moze biti ista osoba????
-        $katedra->pozicija()->sync([
-            $data->sef->id => [
-                'pozicija' => Pozicija::Sef,
-                'datum_od' => $data->sef->datum_od,
-                'datum_do' => $data->sef->datum_do
-            ],
-
-            $data->zamenik->id => [
-                'pozicija' => Pozicija::Zamenik,
-                'datum_od' => $data->zamenik->datum_od,
-                'datum_do' => $data->zamenik->datum_do
-            ]
-        ]);
-
-        return $katedra->fresh();
-    }
 
 
 }
